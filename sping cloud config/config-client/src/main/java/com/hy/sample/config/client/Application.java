fd13dc3293675ac0f3053c7e2117d70627c6b04b
TonyStark888 <huangying2124@163.com>
2019-05-18 08:55:38 +0800

    增加Eureka发现服务

--- sping cloud config/config-client/src/main/java/com/hy/sample/config/client/Application.java
+++ sping cloud config/config-client/src/main/java/com/hy/sample/config/client/Application.java
@@ -2,11 +2,13 @@ package com.hy.sample.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 配置信息客户端
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
