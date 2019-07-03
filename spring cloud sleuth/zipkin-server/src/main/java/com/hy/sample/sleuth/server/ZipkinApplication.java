7dab5b0a39557b10a4f696ea8dcdb7a59facda7f
TonyStark888 <huangying2124@163.com>
2019-07-03 22:31:50 +0800

    sleuth使用rabbitmq+mysql

--- spring cloud sleuth/zipkin-server/src/main/java/com/hy/sample/sleuth/server/ZipkinApplication.java
+++ spring cloud sleuth/zipkin-server/src/main/java/com/hy/sample/sleuth/server/ZipkinApplication.java
@@ -3,12 +3,13 @@ package com.hy.sample.sleuth.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
